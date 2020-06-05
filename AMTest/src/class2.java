18f283f216358c525ec22eb91f50843a03856792
annapurna <annapurna@NOLAPF2LGFWS087.noida.jkt.in>
2020-06-05 13:31:26 +0530

    TG-1 #Ready

--- AMTest/src/class2.java
+++ AMTest/src/class2.java
@@ -3,6 +3,6 @@ public class class2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test Taiga1");
-		System.out.println("Test Taiga2");
+		System.out.println("Test Taaiga2");
 	}
 }
